ac532fee893cdd6d560b96351b8d58cb0a204626
Sofie Van Den Abeele <sofievandenabeele@MacBook-Pro-van-Sofie.local>
2023-04-11 10:48:36 +0200

    Add some Bootstrap

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.layout')
     @section('content')
     <h1>Blog</h1>
-
-    
+    <br>
+    <button type="button" class="btn btn-secondary btn-lg" ><a class="nav-link" href="{{route('register')}}">Back</button>
+    <br>
+    <br>
+    <br>
     
     @foreach ($articles as $article)
-    < x-article-preview-card :article="$article" />
+        <div class="card" style="width: 1000%;">
+            <div class="card-body">
+                <p class="card-text">< x-article-preview-card :article="$article" /></p>
+            </div>
+            <br>
+        </div>
     @endforeach
     @endsection
--- resources/views/components/article-preview-card.blade.php
+++ resources/views/components/article-preview-card.blade.php
@@ -1,13 +1,7 @@
 @props(['article'])
-<button type="button" class="btn btn-secondary btn-lg" ><a class="nav-link" href="{{route('register')}}">Back</button>
-<ul>
-   {{-- {{dd($article->id)}} --}}
    
-    <h1><a class="nav-link" href="/articles/{{$article->id}}">{{$article->name}}</h1></a></li>
+    <h3>{{$article->name}}</h3>
     <p>Email: {{$article->email}}</p>
     <p>Location: {{$article->country}}</p>
-    {{-- <li>Article: {{$article->article}}</li> --}}
-    <a href="/articles/{{$article->id}}">Go to complete article</a>
-
-</ul>
-{{-- articles/{{article->id}} --}}
+    <a href="/articles/{{$article->id}}"> > Go to complete article</a>
+   
