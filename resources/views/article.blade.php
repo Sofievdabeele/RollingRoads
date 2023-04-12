05c09f334b7d39f36a07853faa7cba661c236cd8
Sofie Van Den Abeele <sofievandenabeele@MacBook-Pro-van-Sofie.local>
2023-04-12 15:50:54 +0200

    Add bootstrap to article page

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.layout')
     @section('content')
-
-    <button type="button" class="btn btn-secondary btn-lg" ><a class="nav-link" href="{{route('articles')}}">Back</button>
-   
-    <h1>Article: </h1> 
-    <p>{{$article->article}}</p>
-    
+    <div class="container mx-auto">
+         <h1 class="mt-4 text-center display-1">Trip to {{$article->country}}</h1>
+         <div class="col-md-12 mt-4 text-center">
+            <button type="button" class="btn btn-secondary btn-lg" ><a class="nav-link" href="{{route('register')}}">Back</button>
+        </div>
+        <div class="container mx-auto my-3 mt-5 mb-5">
+            <p style="text-align: justify">{{$article->article}}</p>
+        <div>
+    </div>    
 @endsection 
--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.layout')
     @section('content')
-    <h1>Blog</h1>
-    <br>
-    <button type="button" class="btn btn-secondary btn-lg" ><a class="nav-link" href="{{route('register')}}">Back</button>
-    <br>
-    <br>
-    <br>
-    
-    @foreach ($articles as $article)
-        <div class="card" style="width: 1000%;">
-            <div class="card-body">
-                <p class="card-text">< x-article-preview-card :article="$article" /></p>
-            </div>
-            <br>
+    <div class="container mx-auto">
+        <h1 class="mt-4 text-center display-1">Blog</h1>
+        <div class="col-md-12 text-center">
+            <button type="button" class="btn btn-secondary btn-lg" ><a class="nav-link" href="{{route('register')}}">Back</button>
         </div>
-    @endforeach
+    
+        @foreach ($articles as $article)
+        <div class="container mx-auto my-3">
+            <div class="card" style="width: 100%;">
+                <div class="card-body">
+                    <p class="card-text">< x-article-preview-card :article="$article" /></p>
+                </div>
+           </div>
+        </div>
+        @endforeach
+    </div>
     @endsection
